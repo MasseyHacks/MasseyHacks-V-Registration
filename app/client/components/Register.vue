fc8a05d5db19d7964563d4472c026492602ae4b2
jamesxu123 <36768789+jamesxu123@users.noreply.github.com>
2018-08-06 17:58:44 -0400

    Redesigned Register Page

--- app/client/components/Register.vue
+++ app/client/components/Register.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h2>Register</h2>
+        <!--
         <form @submit.prevent="register">
             <label><input v-model="firstName" placeholder="Master" required autofocus></label><br>
             <label><input v-model="lastName" placeholder="Hax0r" required></label><br>
@@ -9,7 +10,21 @@
             <label><input v-model="password2" placeholder="Just to make sure you remember :)" type="password" required></label><br>
             <button type="submit">login</button>
             <p v-if="error" class="error">{{error}}</p>
-        </form>
+        </form>-->
+        <div id="login-form-box">
+            <h2 class="subtitle">Registration System</h2>
+            <div id="login-form-elements">
+                <form @submit.prevent="register">
+                    <input v-model="firstName" placeholder="First Name" autofocus required>
+                    <input v-model="lastName" placeholder="Last Name" required>
+                    <input v-model="email" placeholder="Email" type="email" required>
+                    <input v-model="password1" placeholder="Password" type="password" required>
+                    <input v-model="password2" placeholder="Confirm Password" type="password" required>
+                    <button type="submit">login</button>
+                    <p v-if="error" class="error">{{error}}</p>
+                </form>
+            </div>
+        </div>
 
         <router-link to="/login">Already have an account? Login here</router-link>
     </div>
