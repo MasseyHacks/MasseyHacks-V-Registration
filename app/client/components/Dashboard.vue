a2aa1a04132fc17a871d577c5b760358c333243a
jamesxu123 <36768789+jamesxu123@users.noreply.github.com>
2018-08-09 17:00:33 -0400

    broken UI dont mind me

--- app/client/components/Dashboard.vue
+++ app/client/components/Dashboard.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        <h2>Dashboard</h2>
+    <div class="app-screen">
+        <div class="title-card">
+            <h2>Dashboard</h2>
+        </div>
+        <div class="grid-layout-main">
+            <p>hello</p>
+        </div>
         <p>Current status: {{$parent.user.status.name.toUpperCase()}}<br>Logged in as: {{$parent.user.fullName}}</p>
     </div>
 </template>
