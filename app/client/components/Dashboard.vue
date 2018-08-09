1704ccc012e1c1afef77185510b47d27386ca7f3
jamesxu123 <36768789+jamesxu123@users.noreply.github.com>
2018-08-09 18:16:34 -0400

    stupid UI

--- app/client/components/Dashboard.vue
+++ app/client/components/Dashboard.vue
@@ -1,24 +1,19 @@
 <template>
     <div class="app-screen">
-        <div class="title-card">
-            <h2>DASHBOARD</h2>
-            <h3>STATUS: {{$parent.user.status.name.toUpperCase()}}</h3>
-        </div>
-        <div class="grid-layout-main">
-            <p>hello</p>
-        </div>
-        <p>Logged in as: {{$parent.user.fullName}}</p>
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                   <h4>hi</h4>
-                </div>
-                <div class="col-md-6">
-                    <h4>hi</h4>
+                <div class="title-card col-md-12">
+                    <h2>DASHBOARD</h2>
+                    <h3>STATUS: {{$parent.user.status.name.toUpperCase()}}</h3>
                 </div>
             </div>
+            <div class="row">
+                <b-card>
+                    <h3 class="card-title">Status</h3>
+                    <p class="card-text">Logged in as: {{$parent.user.fullName}}</p>
+                </b-card>
+            </div>
         </div>
-
     </div>
 </template>
 
