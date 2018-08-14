97616085097abc2f832ab647a66fe2ea62d186a7
jamesxu123 <36768789+jamesxu123@users.noreply.github.com>
2018-08-14 19:23:34 -0400

    UserView polished a bit

--- app/client/components/UserView.vue
+++ app/client/components/UserView.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
         <div class="ui-card dash-card-large">
+            <h3>{{userObj.fullName}}</h3>
+            <div id="detailed-info" style="column-count: 3; column-width: 150px;">
+                <ul>
+                    <li v-for="(value, key) in flatten(userObj)" style="overflow-wrap: break-word; text-align: left;">
+                        {{key}}: {{value}}
+                    </li>
+                </ul>
+            </div>
             <router-link to="/organizer/users"><button class="generic-button-light">Back</button></router-link>
-            <p>{{userObj}}</p>
         </div>
     </div>
 </template>
@@ -33,6 +40,45 @@
                     this.userObj = data
                 }
             })
+        },
+
+        methods: {
+            flatten: function(obj) {
+                var flattened = {}
+                for (var keys in obj) {
+                    if (typeof obj[keys] != "object") {
+                        if (!(keys == "QRCode" || keys == "authSecret" || keys == "_id")) {
+                            flattened[this.prettify(keys)] = obj[keys]
+                        }
+                    } else {
+                        for (var depthKey in obj[keys]) {
+                            flattened[this.prettify(depthKey)] = obj[keys][depthKey]
+                        }
+                    }
+                }
+                return flattened
+            },
+
+            prettify: function(str) {
+/*                var newWordIndex = []
+                for (var i in str.length) {
+                    char = str.charAt(i)
+                    if (isNaN(char * 1)) {
+                        if (char === char.toUpperCase()) {
+                            newWordIndex.push(i)
+                        }
+                    }
+                }
+
+                var newString = ''
+                for (var i in newWordIndex.length) {
+                    if (i == 0) {
+                        newString += str.splice(i, newWordIndex[i])
+                    }
+                }*/
+
+                return str.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })
+            }
         }
 
     }
