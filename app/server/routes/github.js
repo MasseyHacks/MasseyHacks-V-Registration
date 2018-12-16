c8f3808ea5a3fd189543b50dd1657bf540d3b84c
James Xu <xujames007@gmail.com>
2018-12-16 14:45:59 -0500

    Auto pull

---
+++ app/server/routes/github.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+const express = require('express');
+const request = require('request');
+const exec = require('child_process').exec;
+
+require('dotenv').load();
+
+GITHUB_SECRET = process.env.GITHUB_SECRET;
+
+module.exports = function (router) {
+    router.use(express.json());
+
+    //auto pull
+
+    router.get('/pull', function (req, res) {
+        req.on('data', function (chunk) {
+            let sig = "sha1=" + crypto.createHmac('sha1', GITHUB_SECRET).update(chunk.toString()).digest('hex');
+
+            if (req.headers['x-hub-signature'] === sig) {
+                exec('cd ../../../../' + ' && git pull');
+            }
+        });
+
+        res.end();
+    })
+}
