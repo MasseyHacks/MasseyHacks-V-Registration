b727962d86b5bea997360dadcae7e15ba1ce2261
James Xu <xujames007@gmail.com>
2018-12-16 14:29:29 -0500

    More stat fixes

--- app/server/services/stats.js
+++ app/server/services/stats.js
@@ -109,9 +109,11 @@ function calculateStats(callback){
                         throw err;
                     }
 
+
                     newStats.total = users.length;
 
                     async.each(users, function(user, callback){
+                        console.log(user);
                         /*
                         for (var i = 0; i < user.votedBy.length; i++) {
                             if (user.votedBy[i] in votes) {
@@ -155,12 +157,12 @@ function calculateStats(callback){
                             }
 
                             // Count shirt sizes
-                            if (user.profile.shirt in newStats.shirtSizes) {
-                                newStats.shirtSizes[user.profile.shirt] += 1;
+                            if (user.profile.hacker.shirt in newStats.shirtSizes) {
+                                newStats.shirtSizes[user.profile.hacker.shirt] += 1;
                             }
                             // Dietary restrictions
-                            if (user.profile.diet) {
-                                user.profile.diet.forEach(function (restriction) {
+                            if (user.profile.hacker.dietaryRestrictions) {
+                                user.profile.hacker.dietaryRestrictions.forEach(function (restriction) {
                                     if (!newStats.dietaryRestrictions[restriction]) {
                                         newStats.dietaryRestrictions[restriction] = 0;
                                     }
@@ -168,8 +170,8 @@ function calculateStats(callback){
                                 });
                             }
 
-                            if (user.profile.school) {
-                                if (user.profile.school.toLowerCase().includes('massey')) {
+                            if (user.profile.hacker.school) {
+                                if (user.profile.hacker.school.toLowerCase().includes('massey')) {
                                     newStats.demo.massey += 1;
                                 }
                                 else {
@@ -191,12 +193,12 @@ function calculateStats(callback){
                             }
 
                             // Count shirt sizes
-                            if (user.profile.shirt in newStats.confirmedStat.shirtSizes){
-                                newStats.confirmedStat.shirtSizes[user.profile.shirt] += 1;
+                            if (user.profile.hacker.shirt in newStats.confirmedStat.shirtSizes) {
+                                newStats.confirmedStat.shirtSizes[user.profile.hacker.shirt] += 1;
                             }
                             // Dietary restrictions
-                            if (user.profile.diet){
-                                user.profile.diet.forEach(function(restriction){
+                            if (user.profile.hacker.dietaryRestrictions) {
+                                user.profile.hacker.dietaryRestrictions.forEach(function (restriction) {
                                     if (!newStats.confirmedStat.dietaryRestrictions[restriction]){
                                         newStats.confirmedStat.dietaryRestrictions[restriction] = 0;
                                     }
@@ -204,8 +206,8 @@ function calculateStats(callback){
                                 });
                             }
 
-                            if (user.profile.school) {
-                                if (user.profile.school.toLowerCase().includes('massey')) {
+                            if (user.profile.hacker.school) {
+                                if (user.profile.hacker.school.toLowerCase().includes('massey')) {
                                     newStats.confirmedStat.demo.massey += 1;
                                 }
                                 else {
