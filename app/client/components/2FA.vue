cb0527e61d776f6361654c6874ae53643d89859d
Ryan Zhang <ryanzhang0112@gmail.com>
2018-12-20 09:15:00 -0500

    Fix 2FA

--- app/client/components/2FA.vue
+++ app/client/components/2FA.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="main">
+    <div class="main main-login" style="background: url('/img/2.jpg') center;background-size: cover; height: 100vh; width: 100vw;">
         <div class="spacer"></div>
-        <div id="login-form-box" class="vertical-centered">
+        <div id="login-form-box" class="vertical-centered" >
             <h2 class="subtitle">Security Check</h2>
             <p>MasseyHacks Security Policy requires 2 factor authentication to protect the security and privacy of our users.</p>
             <div id="login-form-elements">
                 <form @submit.prevent="codeLogin">
-                    <input v-model="code" type="number" maxlength="6" autofocus required>
+                    <input v-model="code" type="number" maxlength=6 autofocus required>
                     <div id="button-row">
-                        <button type="submit" class="primary-button">sign in</button>
+                        <button type="submit" class="generic-button-dark">sign in</button>
                     </div>
                     <p v-if="error" class="error">{{error}}</p>
                 </form>
