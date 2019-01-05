5b86de8602cd08070eca37837648312eea7ebef4
Ryan <ryanzhang0112@gmail.com>
2019-01-05 10:25:25 -0500

    Disable loading for autosave

--- app/client/components/Application.vue
+++ app/client/components/Application.vue
@@ -367,7 +367,9 @@
             },
             saveApplication(auto) {
                 var parsedForm = this.parseForm(this.applications.hacker, false)
-                swal.showLoading()
+                if (!auto) {
+                    swal.showLoading()
+                }
                 //ajax submit code
                 var data = {};
                 data.userID = Session.getUserID();
