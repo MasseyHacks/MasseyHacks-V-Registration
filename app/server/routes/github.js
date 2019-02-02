02cda7debbed1bc20cb57255e452798f7734a4be
Henry T <henryt.inc@gmail.com>
2019-02-02 17:04:23 -0500

    Reverted pull script

--- app/server/routes/github.js
+++ app/server/routes/github.js
@@ -14,30 +14,26 @@ module.exports = function (router) {
     router.use(express.json());
 
     router.post('/pull', function (req, res) {
-        if (GITHUB_SECRET) {
+        let sig = "sha1=" + crypto.createHmac('sha1', GITHUB_SECRET).update(JSON.stringify(req.body)).digest('hex');
 
-            let sig = "sha1=" + crypto.createHmac('sha1', GITHUB_SECRET).update(JSON.stringify(req.body)).digest('hex');
+        console.log(req.headers['x-hub-signature'], sig)
+        if (req.headers['x-hub-signature'] == sig) {
 
-            console.log(req.headers['x-hub-signature'], sig)
-            if (req.headers['x-hub-signature'] == sig) {
+            var child = spawn('./pull.sh');
 
-                var child = spawn('./pull.sh ' + process.env.GITHUB_ORIGIN +  ' ' + process.env.GITHUB_BRANCH);
+            child.stdout.on('data', function(data) {
+                console.log('child stdout:\n' + data);
 
-                child.stdout.on('data', function (data) {
-                    console.log('child stdout:\n' + data);
+                logger.logAction(-1, -1, 'Webhook source update successful. Commit: ' + req.body['head_commit']['message'], data);
+            });
 
-                    logger.logAction(-1, -1, 'Webhook source update successful. Commit: ' + req.body['head_commit']['message'], data);
-                });
+            res.send("me has pulled");
+            console.log("I PULLED!");
+        } else {
+            logger.logAction(-1, -1, 'Webhook source update rejected', 'IP: ' + (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + ' Headers: ' + (req.rawHeaders).toString());
 
-                res.send("me has pulled");
-                console.log("I PULLED!");
-            } else {
-                logger.logAction(-1, -1, 'Webhook source update rejected', 'IP: ' + (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + ' Headers: ' + (req.rawHeaders).toString());
-
-                res.send("lmao u can't do that");
-                console.log("Pull failed.");
-            }
-
+            res.send("lmao u can't do that");
+            console.log("Pull failed.");
         }
 
         res.end();
